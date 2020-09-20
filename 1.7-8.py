6ea593c7e39f204468934e67f39b8bdfa765099e
1ceman <mechwarrior2027@gmail.com>
2020-09-21 00:28:19 +0300

    1

--- 1.7-8.py
+++ 1.7-8.py
@@ -1,10 +1,18 @@
-z=int(input())
-n=int(input())
-k=0
-for i in range(0,n):
-    j=int(input())
-    k+=j
-if k<=z:
-    print("Покупает")
-elif k>z:
-    print("Не покупает")
+num1 = int(input())
+n1,n2 = 1,1
+count = 0
+result = []
+
+if num1 == 0:
+    print()
+elif num1 == 1:
+    print(n1)
+else:
+    while count < num1:
+        result.append(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count +=1
+list = ' '.join(str(elem) for elem in result)
+print(list)
